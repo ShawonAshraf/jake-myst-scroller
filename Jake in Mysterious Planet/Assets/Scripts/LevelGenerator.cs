a26e376be13c62088f500d6d3cc9d359ce714f41
Shawon Ashraf <shawon13@live.com>
2016-08-10 04:07:49 +0600

    Added leverPiece and LevelGenerator

---
+++ Jake in Mysterious Planet/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class LevelGenerator : MonoBehaviour {
+
+	public static LevelGenerator instance;
+	public List<LevelPiece> levelPrefabs = new List<LevelPiece>();
+	public Transform levelStartPoint;
+	public List<LevelPiece> pieces = new List<LevelPiece>();
+
+	void Awake()
+	{
+		instance = this;
+	}
+
+
+}
