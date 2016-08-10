ef9920bd94b47a7bfba8b63c0d88d3805d64563c
Shawon Ashraf <shawon13@live.com>
2016-08-10 18:50:28 +0600

    Done with init level gen

--- Jake in Mysterious Planet/Assets/Scripts/LevelGenerator.cs
+++ Jake in Mysterious Planet/Assets/Scripts/LevelGenerator.cs
@@ -4,13 +4,72 @@ using System.Collections.Generic;
 public class LevelGenerator : MonoBehaviour {
 
 	public static LevelGenerator instance;
+
+	// this list stores all the levelPrefabs. Pieces will be generated from this
+	// list while the game runs. Procedural generation
 	public List<LevelPiece> levelPrefabs = new List<LevelPiece>();
+
+	// starting point for a piece
 	public Transform levelStartPoint;
+
+	// generated pieces
 	public List<LevelPiece> pieces = new List<LevelPiece>();
 
 	void Awake()
 	{
 		instance = this;
+	}
+
+	void Update() 
+	{
+
+	}
+
+	void Start() 
+	{
+		GenerateInitialPieces();
+	}
+
+
+
+	public void AddPiece() 
+	{
+		int randomIndex = Random.Range(0, levelPrefabs.Count);
+
+		// a temporary levelPiece that will get generated from levelPrefabs
+		LevelPiece tempPiece = (LevelPiece)Instantiate(levelPrefabs[randomIndex]);
+		tempPiece.transform.SetParent(this.transform, false);
+
+		Vector3 spawnPosition = Vector3.zero;
+
+		if(pieces.Count == 0) 
+		{
+			spawnPosition = levelStartPoint.position;
+		}
+		else 
+		{
+			spawnPosition = pieces[pieces.Count - 1].exitPoint.position;
+		}
+
+		tempPiece.transform.position = spawnPosition;
+		pieces.Add(tempPiece);
+	}
+
+	
+	public void RemoveOldestPiece() 
+	{
+		LevelPiece oldestPiece = pieces[0];
+		pieces.Remove(oldestPiece);
+
+		Destroy(oldestPiece.gameObject);
+	}
+
+	public void GenerateInitialPieces() 
+	{
+		for(int i = 0; i < 2; i++) 
+		{
+			AddPiece();
+		}
 	}
 
 
--- Jake in Mysterious Planet/Assets/Scripts/LevelPiece.cs
+++ Jake in Mysterious Planet/Assets/Scripts/LevelPiece.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class LevelPiece : MonoBehaviour {
 	public Transform exitPoint;
